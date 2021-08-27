7eccb56cd50f8459b7777acfb8d808a54e8b7209
Yordan Stayokv <dark.mat73r@gmail.com>
2021-08-27 16:58:24 +0300

    update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    amount: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    expenses: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'Expense'
-        }
-    ]
-});
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		amount: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		expenses: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: "Expense",
+			},
+		],
+	},
+	{
+		capped: { size: 1024 },
+		bufferCommands: false,
+		autoCreate: false, // disable `autoCreate` since `bufferCommands` is false
+	}
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
