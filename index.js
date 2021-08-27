376339aad0099caa6a7830c88cb585d313aab793
Yordan Stayokv <dark.mat73r@gmail.com>
2021-08-27 15:00:36 +0300

    changes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 //Configure the ports and DB connections
-const config = require('./config');
+const config = require("./config");
 
 //Express Setup
-const express = require('express');
+const express = require("express");
 const app = express();
-require('./config/express')(app);
+require("./config/express")(app);
 
 //Mongoose Setup
-require('./config/mongoose')();
+require("./config/mongoose")();
 
 //Router Setup
-const routes = require('./routes');
+const routes = require("./routes");
 app.use(routes);
 
-
-app.listen(config.PORT, () => console.log(`Server is running on port http://localhost:${config.PORT}...`));
+app.listen(config.PORT || 5000, () =>
+	console.log(`Server is running on port http://localhost:${config.PORT}...`)
+);
