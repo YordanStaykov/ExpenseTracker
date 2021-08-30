dff338c6bd3869860b7905cade7713795eb140f3
Yordan Stayokv <dark.mat73r@gmail.com>
2021-08-30 15:07:51 +0300

    migrated DB to MongoDB Atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,22 +1,40 @@
+// const mongoose = require("mongoose");
+
+// function setupMongoose() {
+// 	mongoose.connect(process.env.DB_CONNECTION, {
+// 		useNewUrlParser: true,
+// 		useFindAndModify: false,
+// 		useUnifiedTopology: true,
+// 		useCreateIndex: true,
+// 	});
+
+// 	const db = mongoose.connection;
+
+// 	db.on("error", console.error.bind(console, "connection error:"));
+// 	db.once("open", console.log.bind(console, "Db Connected!"));
+// }
+
+// module.exports = setupMongoose;
+
 const mongoose = require("mongoose");
 
-async function setupMongoose() {
-	try {
-		await mongoose.connect(process.env.DB_CONNECTION, {
-			useNewUrlParser: true,
-			useFindAndModify: false,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const connectionParams = {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+};
+
+function setupMongoose() {
+	mongoose
+		.connect(url, connectionParams)
+		.then(() => {
+			console.log("Connected to database ");
+		})
+		.catch((err) => {
+			console.error(`Error connecting to the database. \n${err}`);
 		});
-		console.log("Server connected to MongoDB");
-	} catch (error) {
-		console.error(error);
-	}
-
-	// const db = mongoose.connection;
-
-	// db.on("error", console.error.bind(console, "connection error:"));
-	// db.once("open", console.log.bind(console, "Db Connected!"));
 }
 
 module.exports = setupMongoose;
